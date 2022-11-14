8522e9f0c0bde24ac6476ba39c27f4c0645ba7a9
Adam Black <ablack3@users.noreply.github.com>
2022-11-14 17:39:43 +0000

    add assert_tables function

--- R/validate.R
+++ R/validate.R
@@ -69,3 +69,90 @@ validate_cdm_rowcounts <- function(cdm) {
   }
   invisible(cdm)
 }
+
+#' @export
+#' @describeIn validate_cdm_rowcounts
+validateCdmRowcounts <- validate_cdm_rowcounts
+
+#' Assert that tables exist in a cdm object
+#'
+#' A cdm object is a list of references to a subset of tables in the OMOP Common Data Model.
+#' If you write a function that accepts a cdm object as a parameter `assert_tables`/`assertTables` will help you check
+#' that the tables you need are in the cdm object, have the correct columns/fields, and (optionally) are not empty.
+#'
+#' @param cdm A cdm object
+#' @param tables A character vector of table names to check.
+#' @param empty.ok Should an empty table (0 rows) be considered an error? TRUE or FALSE (default)
+#' @param add An optional AssertCollection created by `checkmate::makeAssertCollection()` that errors should be added to.
+#'
+#' @return Invisibly returns the cdm object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Use assertTables inside an analysis function to check that tables exist and are populated
+#' countDrugsByGender <- function(cdm) {
+#'   assertTables(cdm, tables = c("person", "drug_era"), empty.ok = FALSE)
+#'
+#'   cdm$person %>%
+#'     dplyr::inner_join(cdm$drug_era, by = "person_id") %>%
+#'     dplyr::count(.data$gender_concept_id, .data$drug_concept_id) %>%
+#'     dplyr::collect()
+#' }
+#'
+#' library(CDMConnector)
+#' con <- DBI::dbConnect(duckdb::duckdb(), dbdir = eunomia_dir())
+#' cdm <- cdm_from_con(con)
+#'
+#' countDrugsByGender(cdm)
+#'
+#' DBI::dbDisconnect(con, shutdown = TRUE)
+#'
+#' }
+assert_tables <- function(cdm, tables, empty.ok = FALSE, add = NULL) {
+  checkmate::assertClass(add, "AssertCollection", null.ok = TRUE)
+  checkmate::assertLogical(empty.ok, len = 1, null.ok = FALSE)
+  checkmate::assertCharacter(tables, min.len = 1, min.chars = 1, null.ok = FALSE)
+  checkmate::assertClass(cdm, "cdm_reference")
+
+  missingTables <- tables[!(tables %in% names(cdm))]
+  existingTables <- tables[tables %in% names(cdm)]
+
+  if (length(missingTables) > 0) {
+    s <- ifelse(length(missingTables) > 1, "s", "")
+    msg <- glue::glue("- {paste(missingTables, collapse = ',' )} table{s} not found in cdm object")
+    if (is.null(add)) rlang::abort(msg) else add$push(msg)
+  }
+
+  # checking of column names will not throw an error if column names exist but are in the wrong order
+  for (nm in existingTables) {
+    # spec_cdm_field is a a global internal package dataframe created in extras/package_maintenance.R
+    expectedColumns <- spec_cdm_field %>% dplyr::filter(.data$cdmTableName == nm) %>% dplyr::pull(.data$cdmFieldName)
+    actualColumns <- cdm[[nm]] %>% head(1) %>% collect() %>% colnames()
+    missingColumns <- dplyr::setdiff(expectedColumns, actualColumns)
+
+    if (length(missingColumns) > 0) {
+      s <- ifelse(length(missingColumns) > 1, "s", "")
+      msg <- glue::glue("- {paste(missingColumns, collapse = ',' )} column{s} not found in cdm table {nm}")
+      if (is.null(add)) rlang::abort(msg) else add$push(msg)
+    }
+  }
+
+  if (!empty.ok) {
+    rowcounts <- purrr::map_dbl(existingTables, function(.) dplyr::tally(cdm[[.]], name = "n") %>% dplyr::pull(.data$n)) %>%
+      rlang::set_names(existingTables)
+
+    empty_tables <- rowcounts[rowcounts == 0]
+
+    if (length(empty_tables) > 0) {
+      msg <- glue::glue("- {paste(names(empty_tables), collapse = ', ')} cdm tables are empty")
+      if (is.null(add)) rlang::abort(msg) else add$push(msg)
+    }
+  }
+
+  invisible(cdm)
+}
+
+#' @export
+#' @describeIn assert_tables
+assertTables <- assert_tables
