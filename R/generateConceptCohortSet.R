f40866729719eb2df1886785bb6c7d9e7f7313d7
edward-burn <9583964+edward-burn@users.noreply.github.com>
2023-07-11 21:36:53 +0100

    export generateConceptCohortSet (#208)
    - export generateConceptCohortSet
    - initial tests passing for duckdb, postgres, and redshift

---
+++ R/generateConceptCohortSet.R
@@ -0,0 +1,214 @@
+#' Internal function to create a new generated cohort set from a list of concept sets
+#'
+#' @description
+#'
+#' Generate a new cohort set from one or more concept sets. Each
+#' concept set will result in one cohort and represent the time during which
+#' the concept was observed for each subject/person. Concept sets can be
+#' passed to this function as:
+#' \itemize{
+#'  \item{A named list of numeric vectors, one vector per concept set}
+#'  \item{A named list of Capr concept sets}
+#' }
+#'
+#' Clinical observation records will be looked up in the respective domain tables
+#' using the vocabulary in the CDM. If a required domain table does not exist in
+#' the cdm object a warning will be given.
+#' Concepts that are not in the vocabulary or in the data will be silently ignored.
+#' If end dates are missing or do not exist, as in the case of the procedure and
+#' observation domains, the the start date will be used as the end date.
+#'
+#' @param cdm A cdm reference object created by `CDMConnector::cdmFromCon` or `CDMConnector::cdm_from_con`
+#' @param conceptSet A named list of numeric vectors or Capr concept sets
+#' @param name The name of the new generated cohort table as a character string
+#' @param overwrite Should the cohort table be overwritten if it already exists? TRUE or FALSE
+#'
+#' @return A cdm reference object with the new generated cohort set table added
+#' @export
+generateConceptCohortSet <- function(cdm,
+                                     conceptSet = NULL,
+                                     name = "cohort",
+                                     overwrite = FALSE) {
+
+  checkmate::checkClass(cdm, "cdm_reference")
+  con <- attr(cdm, "dbcon")
+  checkmate::assertTRUE(DBI::dbIsValid(attr(cdm, "dbcon")))
+
+  if (isFALSE(attr(cdm, "cohort_as_temp"))) {
+    assert_write_schema(cdm)
+  }
+
+  if (!is.list(conceptSet) || methods::is(conceptSet, "ConceptSet")) {
+    conceptSet <- list("unnamed cohort" = conceptSet)
+  }
+
+  checkmate::assertList(conceptSet, min.len = 1, any.missing = FALSE, types = c("numeric", "ConceptSet"), names = "named")
+  checkmate::assertList(conceptSet, min.len = 1, names = "named")
+  CDMConnector::assert_tables(cdm, "concept")
+
+  if (methods::is(conceptSet[[1]], "ConceptSet")) {
+    purrr::walk(conceptSet, ~checkmate::assertClass(., "ConceptSet"))
+
+    df <- dplyr::tibble(cohort_definition_id = seq_along(conceptSet),
+                        cohort_name = names(conceptSet),
+                        df = purrr::map(conceptSet, caprConceptToDataframe)) %>%
+      tidyr::unnest(cols = df) %>%
+      dplyr::select("cohort_definition_id",
+                    "cohort_name",
+                    concept_id = "conceptId",
+                    include_descendants = "includeDescendants",
+                    is_excluded = "isExcluded")
+
+  } else {
+    # conceptSet must be a named list of integer-ish vectors
+    purrr::walk(conceptSet, ~checkmate::assert_integerish(., lower = 0, min.len = 1))
+
+    df <- dplyr::tibble(cohort_definition_id = seq_along(.env$conceptSet),
+                        cohort_name = names(.env$conceptSet),
+                        concept_id = .env$conceptSet) %>%
+      tidyr::unnest(cols = .data$concept_id) %>%
+      dplyr::transmute(.data$cohort_definition_id,
+                       .data$cohort_name,
+                       .data$concept_id,
+                       include_descendants = FALSE,
+                       is_excluded = FALSE)
+  }
+
+  # upload data to the database
+  tempName <- uniqueTableName()
+
+  # TODO fully test temporary argument of dbWriteTable across all database platforms
+  DBI::dbWriteTable(attr(cdm, "dbcon"),
+                    name = inSchema(attr(cdm, "write_schema"),
+                                    tempName, dbms = dbms(con)),
+                    value = df,
+                    overwrite = TRUE)
+
+  if (any(df$include_descendants)) {
+    CDMConnector::assert_tables(cdm, "concept_ancestor")
+  }
+
+  # realize full list of concepts
+  concepts <- dplyr::tbl(attr(cdm, "dbcon"), inSchema(attr(cdm, "write_schema"),
+                                                      tempName,
+                                                      dbms = dbms(con))) %>%
+    { if (any(df$include_descendants)) {
+      dplyr::filter(., .data$include_descendants) %>%
+        dplyr::inner_join(cdm$concept_ancestor, by = c("concept_id" = "ancestor_concept_id")) %>%
+        dplyr::select("cohort_definition_id", "cohort_name", concept_id = "descendant_concept_id", "is_excluded") %>%
+        dplyr::union_all(dplyr::select(dplyr::tbl(attr(cdm, "dbcon"), inSchema(attr(cdm, "write_schema"), tempName, dbms = dbms(con))), "cohort_definition_id", "cohort_name", "concept_id", "is_excluded"))
+    } else . } %>%
+    dplyr::filter(!.data$is_excluded) %>%
+    dplyr::left_join(dplyr::select(cdm$concept, "concept_id", "domain_id"), by = "concept_id") %>%
+    dplyr::select("cohort_definition_id", "cohort_name", "concept_id", "domain_id") %>%
+    dplyr::distinct() %>%
+    CDMConnector::computeQuery(temporary = TRUE)
+
+  DBI::dbRemoveTable(attr(cdm, "dbcon"), name = inSchema(attr(cdm, "write_schema"), tempName, dbms = dbms(con)))
+
+  domains <- concepts %>% dplyr::distinct(.data$domain_id) %>% dplyr::pull() %>% tolower()
+
+  get_domain <- function(domain) {
+    df <- dplyr::tribble(
+      ~domain_id,    ~table_name,            ~concept_id,              ~start_date,                  ~end_date,
+      "condition",   "condition_occurrence", "condition_concept_id",   "condition_start_date",       "condition_end_date",
+      "drug",        "drug_exposure",        "drug_concept_id",        "drug_exposure_start_date",   "drug_exposure_end_date",
+      "procedure",   "procedure_occurrence", "procedure_concept_id",   "procedure_date",             "procedure_date",
+      "observation", "observation",          "observation_concept_id", "observation_date",           "observation_date",
+      "measurement", "measurement",          "measurement_concept_id", "measurement_date",           "measurement_date",
+      "visit",       "visit_occurrence",     "visit_concept_id",       "visit_start_date",           "visit_end_date",
+      "device",      "device_exposure",      "device_concept_id",      "device_exposure_start_date", "device_exposure_end_date"
+    ) %>% dplyr::filter(.data$domain_id == domain)
+
+    if (nrow(df) != 1) {
+      return(NULL)
+    }
+
+    if (!(df$table_name %in% names(cdm))) {
+      rlang::warn(glue::glue("Concept set contains {domain} concepts but the {df$table_name} table is missing from the cdm"))
+      return(NULL)
+    }
+
+    CDMConnector::assert_tables(cdm, df$table_name, empty.ok = TRUE)
+
+    by <- c("concept_id") %>% rlang::set_names(df[["concept_id"]])
+
+    cdm[[df$table_name]] %>%
+      dplyr::inner_join(concepts, by = local(by)) %>%
+      dplyr::transmute(.data$cohort_definition_id,
+                       subject_id = .data$person_id,
+                       cohort_start_date = !!rlang::parse_expr(df$start_date),
+                       cohort_end_date = dplyr::coalesce(!!rlang::parse_expr(df$end_date),
+                                                         !!rlang::parse_expr(df$start_date)))
+  }
+
+  # rowbind results
+  if (!any(is.na(domains)) && length(domains) > 0) {
+    cohort <- purrr::map(domains, get_domain) %>% purrr::reduce(dplyr::union_all)
+  } else {
+    # create the base cohort table
+    cohort <- dplyr::transmute(
+      cdm$observation_period,
+      cohort_definition_id = 0L,
+      subject_id = .data$person_id,
+      cohort_start_date = .data$observation_period_start_date,
+      cohort_end_date = .data$observation_period_end_date) %>%
+      dplyr::filter(1 == 0) %>%
+      computeQuery(temporary = TRUE)
+  }
+
+  checkmate::assertCharacter(attr(cdm, "write_prefix"), len = 1, null.ok = TRUE, min.chars = 1, pattern = "^[a-zA-Z0-9_]+$")
+  cohort_table_name <- paste0(attr(cdm, "write_prefix"), name) # This works when write_prefix is null
+
+  # TODO fully test overwrite in computeQuery
+  cohortRef <- cohort %>%
+    cohort_collapse() %>%
+    CDMConnector::computeQuery(temporary = attr(cdm, "cohort_as_temp"),
+                               schema = attr(cdm, "write_schema"),
+                               name = cohort_table_name,
+                               overwrite = overwrite)
+
+  # create attributes
+  cohortSetRef <- concepts %>%
+    dplyr::distinct(.data$cohort_definition_id, .data$cohort_name) %>%
+    computeQuery(temporary = attr(cdm, "cohort_as_temp"),
+                 schema = attr(cdm, "write_schema"),
+                 name = paste0(cohort_table_name, "_set"),
+                 overwrite = overwrite)
+
+  cohortCountRef <- cohortRef %>%
+    dplyr::group_by(.data$cohort_definition_id) %>%
+    dplyr::summarise(n_subjects = dplyr::n_distinct(.data$subject_id),
+                     n_records = dplyr::n()) %>%
+    dplyr::left_join(cohortSetRef, ., by = "cohort_definition_id") %>%
+    dplyr::mutate(number_subjects = ifelse(is.na(.data$n_subjects), 0L, .data$n_subjects),
+                  number_records  = ifelse(is.na(.data$n_records), 0L, .data$n_records)) %>%
+    dplyr::select("cohort_definition_id", "number_records", "number_subjects") %>%
+    CDMConnector::computeQuery(temporary = attr(cdm, "cohort_as_temp"),
+                               schema = attr(cdm, "write_schema"),
+                               name = paste0(cohort_table_name, "_count"),
+                               overwrite = overwrite)
+
+
+  cohortAttritionRef <- cohortCountRef %>%
+    dplyr::transmute(
+      .data$cohort_definition_id,
+      .data$number_records,
+      .data$number_subjects,
+      reason_id = 1L,
+      reason = "Qualifying initial records",
+      excluded_records = 0L,
+      excluded_subjects = 0L) %>%
+    CDMConnector::computeQuery(temporary = attr(cdm, "cohort_as_temp"),
+                               schema = attr(cdm, "write_schema"),
+                               name = paste0(cohort_table_name, "_attrition"),
+                               overwrite = overwrite)
+
+  cdm[[name]] <- CDMConnector::newGeneratedCohortSet(
+    cohortRef = cohortRef,
+    cohortSetRef = cohortSetRef,
+    cohortAttritionRef = cohortAttritionRef,
+    cohortCountRef = cohortCountRef)
+
+  return(cdm)
+}
--- tests/testthat/test-generateConceptCohortSet.R
+++ tests/testthat/test-generateConceptCohortSet.R
@@ -1,84 +1,109 @@
-# test_that("codelist cohorts", {
-#
-#   skip_if_not_installed("duckdb")
-#   skip_if_not(eunomia_is_available())
-#
-#   con <- DBI::dbConnect(duckdb::duckdb(), eunomia_dir())
-#   cdm_schema = "main"
-#   write_schema = "main"
-#
-#   cdm <- cdm_from_con(con, cdm_schema = cdm_schema, write_schema = c(schema = write_schema, prefix = "tst_"))
-#
-#   cdm <- generateConceptCohortSet(cdm = cdm,
-#                          conceptSet =  list(gibleed = 80809),
-#                          name = "gibleed",
-#                          overwrite = TRUE)
-#
-#   expect_true("GeneratedCohortSet" %in% class(cdm$gibleed))
-#   expect_true(nrow(cdm$gibleed %>%
-#                      dplyr::collect()) >= 1)
-#   expect_true(nrow(cohortAttrition(cdm$gibleed)) >= 1)
-#   expect_true(nrow(cohortSet(cdm$gibleed)) >= 1)
-#   expect_true(nrow(cohortCount(cdm$gibleed)) >= 1)
-#
-#   # overwrite cohort table
-#   cdm <- generateConceptCohortSet(cdm = cdm,
-#                                   conceptSet =  list(gibleed = 80809),
-#                                   name = "gibleed",
-#                                   overwrite = TRUE,
-#                                   computeAttrition = TRUE)
-#   expect_true(nrow(cdm$gibleed %>%
-#                      dplyr::collect()) >= 1)
-#   # error if overwrite is false
-#   expect_error(generateConceptCohortSet(cdm = cdm,
-#                            conceptSet =  list(gibleed = 80809),
-#                            name = "gibleed",
-#                            overwrite = FALSE,
-#                            computeAttrition = TRUE))
-#
-#   # check we have the people we would expect
-#   expect_true(all(sort(cdm$condition_occurrence %>%
-#     dplyr::filter(condition_concept_id==80809) %>%
-#     dplyr::pull("person_id")) ==
-#     sort(cdm$gibleed %>%
-#     dplyr::pull("subject_id"))))
-#
-#
-#
-#   # concept cohorts based on multiple domains
-#   conceptList <- list("a" = c(4336464), # procedure
-#                       "b" = c(4112343, # condition
-#                               4336464, # procedure
-#                               3006322))  # measurement
-#   cdm <- generateConceptCohortSet(cdm, conceptSet = conceptList,
-#                                            name = "test_cohort_2",
-#                                   overwrite = TRUE)
-#
-#   # check we have the people we would expect
-#   # cohort 1
-#   expect_true(all(sort(cdm$procedure_occurrence %>%
-#                          dplyr::filter(procedure_concept_id==4336464) %>%
-#                          dplyr::pull("person_id")) ==
-#                     sort(cdm$test_cohort_2 %>%
-#                            dplyr::filter(cohort_definition_id == "1") %>%
-#                            dplyr::pull("subject_id"))))
-#   # cohort 2
-#   expect_true(all(sort(unique(c(cdm$condition_occurrence %>%
-#                          dplyr::filter(condition_concept_id==4112343 ) %>%
-#                          dplyr::pull("person_id"),
-#                         cdm$procedure_occurrence %>%
-#                          dplyr::filter(procedure_concept_id==4336464) %>%
-#                          dplyr::pull("person_id"),
-#                   cdm$measurement %>%
-#                     dplyr::filter(measurement_concept_id==3006322) %>%
-#                     dplyr::pull("person_id"))))==
-#                     sort(unique(cdm$test_cohort_2 %>%
-#                            dplyr::filter(cohort_definition_id == "2") %>%
-#                            dplyr::pull("subject_id")))))
-#
-# cdmDisconnect(cdm)
-# })
+dbToTest <- c(
+  "duckdb"
+  ,"postgres"
+  ,"redshift"
+  # ,"sqlserver"
+  # ,"oracle"  # requires development version of dbplyr
+  # ,"snowflake" invalid identifier 'COHORT_DEFINITION_ID'
+  # ,"bigquery" Type not found: VARCHAR at [4:10] [invalidQuery]
+)
 
+test_concept_cohort_perm <- function(con,
+                                cdm_schema,
+                                write_schema) {
 
+  test_prefix_rand <- paste0("test_",
+                             paste0(sample(letters, size = 5, replace = TRUE),
+                                    collapse = ""))
+  cdm <- cdm_from_con(con,
+                      cdm_schema = cdm_schema,
+                      write_schema = c(schema = write_schema,
+                                       prefix = test_prefix_rand))
+
+  # create permanent cohort tables
+  attr(cdm, "cohort_as_temp") <- FALSE
+  cdm <- generateConceptCohortSet(cdm = cdm,
+                                  conceptSet = list(gibleed = 80809),
+                                  name = "gibleed",
+                                  overwrite = TRUE)
+
+  # check tables we expected have been created
+  expect_true(paste0(test_prefix_rand, "gibleed") %in%
+                CDMConnector::listTables(con = con, schema = write_schema))
+  expect_true(paste0(test_prefix_rand, "gibleed_attrition") %in%
+                CDMConnector::listTables(con = con,
+                                         schema = write_schema))
+  expect_true(paste0(test_prefix_rand, "gibleed_count")  %in%
+                CDMConnector::listTables(con = con,
+                                         schema = write_schema))
+  expect_true(paste0(test_prefix_rand, "gibleed_set")  %in%
+                CDMConnector::listTables(con = con,
+                                         schema = write_schema))
+
+  # check that overwrite works as expected
+  expect_no_error(generateConceptCohortSet(cdm = cdm,
+                                    conceptSet =  list(gibleed = 80809),
+                                    name = "gibleed",
+                                    overwrite = TRUE))
+  expect_error(generateConceptCohortSet(cdm = cdm,
+                             conceptSet =  list(gibleed = 80809),
+                             name = "gibleed",
+                             overwrite = FALSE))
+
+  # check we have the people we would expect
+  expect_true(all(sort(unique(cdm$condition_occurrence %>%
+      dplyr::filter(condition_concept_id==80809) %>%
+      dplyr::pull("person_id"))) ==
+      sort(unique(cdm$gibleed %>%
+      dplyr::pull("subject_id")))))
+
+    # concept cohorts based on multiple domains
+      conceptList <- list("a" = c(4336464), # procedure
+                          "b" = c(4112343, # condition
+                                  4336464, # procedure
+                                  3006322))  # measurement
+      cdm <- generateConceptCohortSet(cdm, conceptSet = conceptList,
+                                               name = "test_cohort_2",
+                                      overwrite = TRUE)
+
+      # check we have the people we would expect
+      # cohort 1
+      expect_true(all(sort(unique(cdm$procedure_occurrence %>%
+                             dplyr::filter(procedure_concept_id==4336464) %>%
+                             dplyr::pull("person_id"))) ==
+                        sort(unique(cdm$test_cohort_2 %>%
+                               dplyr::filter(cohort_definition_id == "1") %>%
+                               dplyr::pull("subject_id")))))
+      # cohort 2
+      expect_true(all(sort(unique(c(cdm$condition_occurrence %>%
+                             dplyr::filter(condition_concept_id==4112343 ) %>%
+                             dplyr::pull("person_id"),
+                            cdm$procedure_occurrence %>%
+                             dplyr::filter(procedure_concept_id==4336464) %>%
+                             dplyr::pull("person_id"),
+                      cdm$measurement %>%
+                        dplyr::filter(measurement_concept_id==3006322) %>%
+                        dplyr::pull("person_id"))))==
+                        sort(unique(cdm$test_cohort_2 %>%
+                               dplyr::filter(cohort_definition_id == "2") %>%
+                               dplyr::pull("subject_id")))))
+
+  CDMConnector::dropTable(cdm = cdm,
+                          name = dplyr::starts_with(test_prefix_rand))
+}
+
+for (dbtype in dbToTest) {
+  test_that(glue::glue("{dbtype} - generateCohortSet"), {
+
+    con <- get_connection(dbtype)
+    cdm_schema <- get_cdm_schema(dbtype)
+    write_schema <- get_write_schema(dbtype)
+    skip_if(any(write_schema == "") || any(cdm_schema == "") || is.null(con))
+    test_concept_cohort_perm(con,
+                             cdm_schema = cdm_schema,
+                             write_schema = write_schema)
+    disconnect(con)
+  })
+}
 
 
