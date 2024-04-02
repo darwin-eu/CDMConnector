49fa189109cf62a18366818a457072b8f43a4e6b
Adam Black <ablack3@users.noreply.github.com>
2024-04-02 13:43:01 +0200

    autocommit

--- tests/testthat/test-deprecations.R
+++ tests/testthat/test-deprecations.R
@@ -1,4 +1,5 @@
 test_that("computeQuery gives warning", {
+  testthat::skip_if_not_installed("duckdb")
   con <- DBI::dbConnect(duckdb::duckdb(eunomia_dir()))
   cdm <- cdm_from_con(con, "main", "main")
 
