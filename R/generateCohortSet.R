1e77532c453efe9f7b6d40206736ba5b5e443cd3
ablack3 <ablack3@users.noreply.github.com>
2025-02-11 12:50:45 +0000

    don't use na.rm in n_distinct.

--- R/generateCohortSet.R
+++ R/generateCohortSet.R
@@ -643,7 +643,7 @@ computeAttritionTable <- function(cdm,
   # Difference can be due to collapsing of cohort records in the cohort eras Circe step.
   finalCounts <- dplyr::tbl(con, .inSchema(schema, cohortStem, dbms(con))) %>%
     dplyr::group_by(.data$cohort_definition_id) %>%
-    dplyr::summarise(n_records = dplyr::n(), n_subjects = dplyr::n_distinct(.data$subject_id, na.rm = TRUE)) %>%
+    dplyr::summarise(n_records = dplyr::n(), n_subjects = dplyr::n_distinct(.data$subject_id)) %>%
     dplyr::collect()
 
   lastAttritionRow <- dplyr::slice_max(attrition, n = 1, order_by = .data$reason_id, by = "cohort_definition_id")
